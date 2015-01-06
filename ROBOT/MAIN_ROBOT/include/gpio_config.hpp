d21ca48a2a96195ef7e0bfcbcc443a0b6c8d749a
Tokunn <kaseikappatokunn2@yahoo.co.jp>
2015-01-06 17:42:19 +0900

    gpio_config

---
+++ ROBOT/MAIN_ROBOT/include/gpio_config.hpp
@@ -0,0 +1,18 @@
+// LEFT Normal rotation
+#define LEFT_NORMAL_ROTATION 45     // P8_11
+
+// LEFT Reverse rotation
+#define LEFT_REVERS_ROTATION 44     // P8_12
+
+// LEFT Speed
+#define LEFT_SPEED           23     // P8_13
+
+
+// RIGHT Normal rotation
+#define RIGT_NORMAL_ROTATION 15     // P9_24
+
+// RIGHT Reverse rotation
+#define RIGT_REVERS_ROTATION 2      // P9_22
+
+// RIGHT Speed
+#define RIGT_SPEED           3      // P9_21
